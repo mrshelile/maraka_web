aba6a4f6c696f75d7db5a806b2f7aa66266e66b2
ghost <ghost@localhost.localdomain>
2024-09-04 12:36:22 +0200

    adding placeholders

--- src/pages/market/components/Banner.vue
+++ src/pages/market/components/Banner.vue
@@ -1,8 +1,8 @@
 <template>
-     <!-- <div v-if="banners.length==0" class="w-screen h-72">
+     <div v-if="banners.length==0" class="w-screen h-72">
          <img :src="pictures[0]['src']" :alt="pictures[0]['alt']" class="object-cover h-full w-full"/>
-     </div> -->
-    <fwb-carousel :pictures="$props.banners" />
+     </div>
+    <fwb-carousel v-else :pictures="$props.banners" />
 </template>
 <script lang="ts">
 import { FwbCarousel } from 'flowbite-vue'
@@ -14,6 +14,13 @@ export default {
             type:Array<any>,
             required:true
         } as any
+    },
+    data(vm) {
+        return {
+            pictures:[
+            {src: buyandsell, alt: 'placeholder'},
+            ],
+        }
     },
     components:{
         FwbCarousel,
